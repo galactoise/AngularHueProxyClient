99347d7fa9ce93224eba6cc7b705bf00d2c3c44f
Eric <galactoise@gmail.com>
2014-07-17 23:19:57 -0700

    Cleanup of various debug functions and variables, and additional REST calls implemented

--- hue-proxy-client.js
+++ hue-proxy-client.js
@@ -1,28 +1,19 @@
 (function(){
 	var app = angular.module('hueProxyClient',[]);
 	
-	//Deprecate this - service is better way to go
-	app.controller('hueProxyController',['$http','$log',function($http,$log){
-		
-		
-		
-		this.getLight2 = function(lights){
-			$http.get(serviceUrl + '/api/newdeveloper/lights/2').success(function(data){
-				angular.extend(lights[2],data);
-			}).error(function(data){
-				$log.log("ERROR");
-			});
-		};
-	}]);
-	
 	//this module should really just be the service below, which will be expanded to act as a client
 	app.factory('hueProxyClientService',['$http','$log',function($http,$log){
+		var getLights = function(){
+			$log.log("Getting basic lights info...");
+			return $http.get(serviceUrl + '/api/newdeveloper/lights');
+		};
 		var getLightById = function(id){
 			$log.log("Getting light with id = '" + id + "'...");
-			return $http.get(serviceUrl + '/api/newdeveloper/lights/2');
+			return $http.get(serviceUrl + '/api/newdeveloper/lights/' + id);
 		};
 		return {
-			getLightById: function(id) { return getLightById(id);}
+			getLightById: function(id) { return getLightById(id);},
+			getLights: function(){ return getLights();}
 		};
 	}]);
 })();
