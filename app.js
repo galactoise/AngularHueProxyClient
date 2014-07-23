a1d900b7484dab5c5ffd1e2f4ff4726261b12fa7
Eric <galactoise@gmail.com>
2014-07-22 23:45:47 -0700

    Started converting to use actual proxy, instead of direct to Hue bridge

--- app.js
+++ app.js
@@ -1,19 +1,32 @@
 (function(){
-	serviceUrl = "http://192.168.1.252";
+//	var serviceUrl = "http://192.168.1.252/api/newdeveloper"; //Direct-to-Hue
+	var serviceUrl = "http://127.0.0.1:9080/HueProxy/rest"; //HueProxy
 	var app = angular.module('testApp',['hueProxyClient']);
 	app.controller('TestController',['$log','$scope','hueProxyClientService',function($log, $scope, hueProxyClientService){
 		$scope.lights = null;
 		
+		hueProxyClientService.setServiceUri(serviceUrl);
+		
 		hueProxyClientService.getLights().success(function(data){
+			
+			/**
+			 * This is a stupid hack to convert between direct hue response, and proxy response
+			 * It should be removed once hue-proxy-client reliably talks only to the proxy
+			 * When it is removed, references by light id will also need to be changed to be id - 1
+			 */
+			if(data instanceof Array){
+				data.unshift(null);
+			}
+			
 			$scope.lights = data;
 		});
 		
 		this.getLights = function(){
-			$log.log($scope.lights);
 			return $scope.lights;
 		};
 		
 		this.getStateFromLight = function(lightObject){
+			if(lightObject === null){ return null;}
 			return lightObject.state;
 		};
 		
--- hue-proxy-client.js
+++ hue-proxy-client.js
@@ -3,17 +3,34 @@
 	
 	//this module should really just be the service below, which will be expanded to act as a client
 	app.factory('hueProxyClientService',['$http','$log',function($http,$log){
+		var serviceUri = null;
+		
+		var LIGHTS_PATH = "/lights";
+		var LIGHTS_BY_ID_PATH = "/lights/%s";
+		
+		var setServiceUri = function(serviceUri2){
+			serviceUri = serviceUri2;
+		};
+		
+		var getServiceUri = function(){
+			return serviceUri;
+		};
+		
 		var getLights = function(){
 			$log.log("Getting basic lights info...");
-			return $http.get(serviceUrl + '/api/newdeveloper/lights');
+			return $http.get(serviceUri + LIGHTS_PATH);
 		};
 		var getLightById = function(id){
+			var url = serviceUri + LIGHTS_BY_ID_PATH.replace(/%s/g,id);
 			$log.log("Getting light with id = '" + id + "'...");
-			return $http.get(serviceUrl + '/api/newdeveloper/lights/' + id);
+			$log.log("Url: '" + url + "'...");
+			return $http.get(url);
 		};
 		return {
 			getLightById: function(id) { return getLightById(id);},
-			getLights: function(){ return getLights();}
+			getLights: function(){ return getLights();},
+			getServiceUri: function(){ return getServiceUri(); },
+			setServiceUri: function(serviceUri){ setServiceUri(serviceUri); }
 		};
 	}]);
 })();
